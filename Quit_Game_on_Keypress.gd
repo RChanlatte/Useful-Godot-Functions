31d510f170475e1d25e0291412254c82744802b3
Ryan Chanlatte <ryan.chanlatte@yahoo.com>
2018-03-05 20:19:11 -0500

    Initial Commit

---
+++ Quit_Game_on_Keypress.gd
@@ -0,0 +1,11 @@
+func quit_game():
+	# closes the root scene, quitting the game.
+	get_tree().quit()
+	
+# "func _input(event)" is a special function in Godot that polls the inputting
+# device for information every frame.
+func _input(event):
+	# if the ESC key is pressed, call the "quit_game" function, ending the game
+	if(event.is_action_pressed("ui_cancel")):
+		print("The game is closing!")
+		quit_game()
